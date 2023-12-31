5953933486cbb8066263260fe68e7d27eceb6220
komal <singh777komal@gmail.com>
2023-12-31 10:12:16 +0530

    connection_b/w_mysqlandpython

--- model/user_model.py
+++ model/user_model.py
@@ -1,5 +1,13 @@
-
+import mysql.connector
 
 class user_model():
-    def user_signup_model(self):
-        return 'user_signup_model'
+
+    def __init__(self):
+        try:
+            con=mysql.connector.connect(host='localhost', user='root', password='admin', database='flask_practice')
+            print('connection succesfull')
+        except:
+            print('some error')
+
+    def user_getall_model(self):
+        return 'user_getall_model'
