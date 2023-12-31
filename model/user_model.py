0e275bf4df27e1bb4b3c89b4629628395030ac7d
komal <singh777komal@gmail.com>
2023-12-31 11:01:35 +0530

    read_operation_GETmethod

--- model/user_model.py
+++ model/user_model.py
@@ -1,13 +1,17 @@
 import mysql.connector
+import json
 
 class user_model():
 
     def __init__(self):
         try:
-            con=mysql.connector.connect(host='localhost', user='root', password='admin', database='flask_practice')
+            self.con=mysql.connector.connect(host='localhost', user='root', password='admin', database='flask_practice')
+            self.cur=self.con.cursor(dictionary=True)
             print('connection succesfull')
         except:
             print('some error')
 
     def user_getall_model(self):
-        return 'user_getall_model'
+        self.cur.execute("SELECT * FROM USERS")
+        res=self.cur.fetchall()
+        return json.dumps(res)
