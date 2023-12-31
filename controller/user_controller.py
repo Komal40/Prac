cf79be4ec415a79339fbbe47249b9705ec971acd
komal <singh777komal@gmail.com>
2023-12-31 22:01:56 +0530

    POSTmethod

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,7 +1,14 @@
 from app import app
 from model.user_model import user_model
+from flask import request
+
 obj=user_model()
 
 @app.route('/user/getall')
 def user_getall_controller():
     return obj.user_getall_model()
+
+@app.route('/user/addone', methods=['POST'])
+def add_one_controller():
+    return obj.add_one_model(request.form)
+
--- model/user_model.py
+++ model/user_model.py
@@ -2,16 +2,22 @@ import mysql.connector
 import json
 
 class user_model():
-
     def __init__(self):
         try:
             self.con=mysql.connector.connect(host='localhost', user='root', password='admin', database='flask_practice')
+            self.con.autocommit=True
             self.cur=self.con.cursor(dictionary=True)
             print('connection succesfull')
         except:
             print('some error')
 
+
     def user_getall_model(self):
         self.cur.execute("SELECT * FROM USERS")
         res=self.cur.fetchall()
         return json.dumps(res)
+    
+
+    def add_one_model(self, data):
+        self.cur.execute(f"INSERT INTO users(name, email, phone) VALUES('{data['name']}', '{data['email']}', '{data['phone']}')")
+        return 'User connected successfully'
